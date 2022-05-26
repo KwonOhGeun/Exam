2ebee5688e13ff634bf2164d1c521c3497e5a92d
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-05-26 18:11:23 +0900

    2022.05.26

---
+++ src/main/java/MethodTest01.java
@@ -0,0 +1,21 @@
+class MethodTest01 {
+    public static void Java(){
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("이협건교수님 프로그래밍언어실습");
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        Java();
+        String name ="이협건";
+        System.out.println("name : " + name);
+
+        Java();
+        String classname = "프로그래밍언어실습";
+        System.out.println("classname : " + classname);
+
+        Java();
+    }
+
+}
