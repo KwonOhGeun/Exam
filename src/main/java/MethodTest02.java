999bda9ac744ad792a546514111a2853f9c233ed
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-05-27 15:49:33 +0900

    2022.05.27 method02

---
+++ src/main/java/MethodTest02.java
@@ -0,0 +1,8 @@
+public class MethodTest02 {
+    public static void main(String[] args) {
+        System.out.println(sum(10,6));
+    }
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
