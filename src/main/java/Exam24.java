5a222321bb3dc28a2214dcb235ddda02f50a904b
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-06-02 21:37:08 +0900

    2022.06.02 예제

---
+++ src/main/java/Exam24.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exam24 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String in, out = "";
+        char ch;
+        int count, i;
+        int diff = 'a' - 'A';
+
+        System.out.printf("문자열 입력 : ");
+        in = s.nextLine();
+
+        count = in.length();
+
+        for (i = 0; i < count; i++) {
+            ch = in.charAt(i);
+
+            if (('A' <= ch) && (ch <= 'Z'))
+                out += (char) (ch + diff);
+            else if (('a' <= ch) && (ch <= 'z'))
+                out += (char) (ch - diff);
+            else
+                out += (char) ch;
+            }
+
+
+                System.out.printf("변환된 문자열 ==> %s\n", out);
+            }
+        }
+
