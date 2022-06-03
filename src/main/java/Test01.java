be7dc333171cd8f1c71fc00b8fc79af83f549ba6
KwonOhGeun <101384390+KwonOhGeun@users.noreply.github.com>
2022-06-03 23:00:05 +0900

    2022.06.03 sort

--- src/main/java/Test01.java
+++ src/main/java/Test01.java
@@ -1,3 +1,24 @@
-public class Test01
-{
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Test01{
+        public static void main(String[] args) {
+
+                ArrayList<String> list = new ArrayList<>(Arrays.asList("C", "A", "B", "a"));
+                System.out.println("원본 : " + list); // [C, A, B, a]
+
+                Collections.sort(list);
+                System.out.println("오름차순 : " + list); //[A, B, C, a]
+
+                Collections.sort(list, Collections.reverseOrder());
+                System.out.println("내림차순 : " + list); // [a, C, B, A]
+
+                Collections.sort(list, String.CASE_INSENSITIVE_ORDER);
+                System.out.println("대소문자 구분없이 오름차순 : " + list); //[a, A, B, C]
+
+                Collections.sort(list, Collections.reverseOrder(String.CASE_INSENSITIVE_ORDER));
+                System.out.println("대소문자 구분없이 내림차순" + list); // [C, B, a, A]
+
         }
+}
